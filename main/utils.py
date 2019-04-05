c9bad0e2226ff66871bb89e7e031c66ae1846f72
dev-prakhar <prakhars1996@gmail.com>
2019-04-06 00:21:55 +0530

    Added utils method to fetch git logs for directory

--- main/utils.py
+++ main/utils.py
@@ -1,11 +1,18 @@
 import subprocess
 
+from constants import GIT_VALID_DIRECTORY_COMMAND, GIT_LOG_COMMAND, NON_ZERO_CODE_ERROR, COMMIT
+from exceptions import NonZeroExitCodeException
+
+
 def execute_command(command):
     """
     Method to execute command in shell
     :param command:
     """
-    return subprocess.call(command, shell=True)
+    exit_code = subprocess.call(command, shell=True)
+    if exit_code:
+        raise NonZeroExitCodeException(NON_ZERO_CODE_ERROR)
+    return exit_code
 
 def get_output(command):
     """
@@ -22,3 +29,42 @@ def get_cleaned_log(raw_log):
     :return: list of string without stray spaces and new lines
     """
     return [line.strip() for line in raw_log if line]
+
+def get_git_log_contents(directory, log_count):
+    """
+    Method to get git log contents for the given directory and log count
+    :param directory
+    :param log_count:
+    :return: list of git log lines
+    """
+    execute_command(GIT_VALID_DIRECTORY_COMMAND.format(dir=directory))  # exception if directory is not git initialised
+    git_log = get_output(GIT_LOG_COMMAND.format(dir=directory, number=log_count)).splitlines(keepends=True)
+
+    git_logs = []
+    single_log = ''
+    for log in git_log:
+        if log.startswith(COMMIT):
+            if single_log:
+                git_logs.append(single_log)
+                single_log = ''
+        single_log += log
+
+    if single_log:
+        git_logs.append(single_log)
+
+    return git_logs
+
+def get_string_after_n_space(text, n):
+    """
+    Method to return string after the nth space
+
+    Input --> 'test1 test2 test3', 1
+    Output --> test2 test3
+
+    Input --> 'test1 test2 test3, 2
+    Output --> test3
+    :param text
+    :param n:
+    :return: string after nth space
+    """
+    return text.split(' ', n)[-1].strip()
