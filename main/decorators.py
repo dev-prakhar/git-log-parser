efb53319e93b80e6642eece25d8097aad3040e5f
dev-prakhar <prakhars1996@gmail.com>
2019-04-06 00:14:27 +0530

    Added exception decorator

---
+++ main/decorators.py
@@ -0,0 +1,15 @@
+from exceptions import NonZeroExitCodeException, GitLogParsingException
+from loggers import Logger
+
+
+def catch_exception(func):
+    """
+    Decorator that catches exception and exits the code if needed
+    """
+    def exit_if_failed(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except (NonZeroExitCodeException, GitLogParsingException) as exception:
+            Logger.error(exception)
+            quit()
+    return exit_if_failed
