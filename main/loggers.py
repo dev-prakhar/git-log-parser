9b41ba3415aff5761b77244a275ca96491a178f6
dev-prakhar <prakhars1996@gmail.com>
2019-04-06 00:15:14 +0530

    Added Custom Logger

---
+++ main/loggers.py
@@ -0,0 +1,17 @@
+class Logger:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    @classmethod
+    def log(cls, message):
+        print(cls.OKGREEN + message + cls.ENDC)
+
+    @classmethod
+    def error(cls, message):
+        print(cls.FAIL + message + cls.ENDC)
